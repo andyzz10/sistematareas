529394ce4ed6675764fb5f1e2ccdf9f4e6bd4cf5
andyzz10 <andyjavierzambrano89@gmail.com>
2026-04-22 01:43:53 -0500

    cuenta

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ python manage.py collectstatic --no-input
 
 # Correr migraciones
 python manage.py migrate
+# Crear superusuario de forma no interactiva (Truco para el plan gratis)
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='tu_usuario').exists() or User.objects.create_superuser('Andyz15', 'admin@example.com', '13080907')"
