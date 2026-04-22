e7a0ae2cfa63022ce0da42a2e85acc1459ffc55b
andyzz10 <andyjavierzambrano89@gmail.com>
2026-04-22 00:53:09 -0500

    Configuración lista para Render

---
+++ sistematareas/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Instalar dependencias
+pip install -r requirements.txt
+
+# Recolectar archivos estáticos
+python manage.py collectstatic --no-input
+
+# Correr migraciones
+python manage.py migrate
